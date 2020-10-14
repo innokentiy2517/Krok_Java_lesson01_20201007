2d3761f14a096da0d46c774d95e29ae88bea0f46
innokentiy2517 <innokentiy2517@gmail.com>
2020-10-14 13:38:41 +0800

    Changed Logic

--- Homework_First/src/Logic/Logic.java
+++ Homework_First/src/Logic/Logic.java
@@ -1,5 +1,14 @@
 package Logic;
 
+/*
+1. Выводить либо число, либо fizz\buzz\fizzbuzz
+2. Каждое из условий if выполняется для каждого числа.
+   Возможно оптимизировать таким образом,
+   чтобы при выполнении одного из условий,
+   следующее уже не проверялось.
+   Для этого нужно поменять порядок проверок и использовать цепочки if-else-if.
+ */
+
 public class Logic {
     //Проверка делимости на 3
     boolean Divided_By_Three(int i){
@@ -18,10 +27,23 @@ public class Logic {
 
     //Проверка делимости
     public String Check_Division(int i) {
-        String result = "";
-        if (Divided_By_Three(i)) result =  "Fizz";
+        String result = Integer.toString(i);
+        /*if (Divided_By_Three(i)) result =  "Fizz";
         if(Divided_By_Five(i)) result =  "Buzz";
-        if(Divided_By_Both(i)) result = "FizzBuzz";
+        if(Divided_By_Both(i)) result = "FizzBuzz";*/
+        if (Divided_By_Both(i)) {
+            result = "FizzBuzz";
+        }
+        else
+        {
+            if (Divided_By_Five(i)){
+                result = "Buzz";
+            }
+            else
+            {
+                if (Divided_By_Three(i)) result = "Fizz";
+            }
+        }
         return result;
     }
 }
